0d414875abcc63554e5df116ab0b2a9350344fad
fmaredia <farhana.maredia@gmail.com>
2015-07-06 17:27:04 -0500

    Implemented bcrypt on passwords.

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,16 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
-var Promise = require('bluebird');
+// var Promise = require('bluebird');
 
 var User = db.Model.extend({
+  tableName: 'users',
+  initialize: function(){
+    this.on('creating', function(model, attrs, options){
+      var newPass = bcrypt.hashSync(model.get('password'));
+      model.set('password', newPass);
+    });
+  }
 });
 
 module.exports = User;
+
--- shortly.js
+++ shortly.js
@@ -23,24 +23,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create',
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links',
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links',
 function(req, res) {
   var uri = req.body.url;
 
@@ -78,8 +78,30 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
+app.get("/login", function(req, res){
+  res.render("login");
+})
 
+app.post("/login", function(req, res){
+  // check if user & pw exist in the database (check hashes)
+  // return a sessionID or an error
+})
 
+app.get("/signup", function(req, res){
+  res.render("signup")
+})
+
+app.post("/signup", function(req, res){
+  var userName = req.body.username
+  var pass = req.body.password
+
+  var nUser = new User({username: userName, password: pass})
+
+  nUser.save().then(function(newUser){
+    Users.add(newUser)
+    res.send(201, "Signup was successful!")
+  })
+})
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
 // assume the route is a short code and try and handle it here.
